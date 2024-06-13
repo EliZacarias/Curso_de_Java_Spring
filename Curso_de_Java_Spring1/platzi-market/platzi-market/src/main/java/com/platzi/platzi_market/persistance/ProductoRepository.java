d6facd3b5a35e693e9d84653aa7413643dc45ef1
Eli Zacarias <jfzzpro@gmail.com>
2024-06-13 17:27:42 -0600

    Introducimos los query methods en la interface y las llamamos en la entidad crudrepository

--- Curso_de_Java_Spring1/platzi-market/platzi-market/src/main/java/com/platzi/platzi_market/persistance/ProductoRepository.java
+++ Curso_de_Java_Spring1/platzi-market/platzi-market/src/main/java/com/platzi/platzi_market/persistance/ProductoRepository.java
@@ -4,12 +4,22 @@ import com.platzi.platzi_market.persistance.crud.ProductoCrudRepository;
 import com.platzi.platzi_market.persistance.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     public List<Producto> getAll(){
         return (List<Producto>)productoCrudRepository.findAll(); //tenemos que convertir(cast) el iterable de findAll
                                             // al tipo List<Productos>, que es lo que stamos solicitando, una lista de los productos en la BD
+    }
 
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);  //se utliza el metodo de la interfaz en esta nueva clase
+                                                //y se realiza ordenado de manera ascendente
+
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
     }
 }
--- Curso_de_Java_Spring1/platzi-market/platzi-market/src/main/java/com/platzi/platzi_market/persistance/crud/ProductoCrudRepository.java
+++ Curso_de_Java_Spring1/platzi-market/platzi-market/src/main/java/com/platzi/platzi_market/persistance/crud/ProductoCrudRepository.java
@@ -1,9 +1,28 @@
 package com.platzi.platzi_market.persistance.crud;
 
 import com.platzi.platzi_market.persistance.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {//recibe dos parametros que es la tabla y el tipo de la clave primaria(integer)
+
+    //Para trabajar con QueryMethods aqui se crean
+
+    //se puede utilizar de esta manera
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?",nativeQuery = true)
+    //ó
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria); //deseamos consultaar una lista de productos por medio del idCategoria
+                                    //Se pone IdCategoria y no idCategoria, porque tenemos que respetar el camel case
+                                    // se pone el mismo nombre a la variable de entrada
+                                    // se pueden nombrar de cualquier manera, en ves de findByIdCategoria podría ser getByCategoria
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado); //tambien soportan los operador opcionales de los nuevas v de java
+
+
+
 
 
 }
