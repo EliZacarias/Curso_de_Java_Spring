21200abc42400219ffaeca31976820aae9e94a1a
Eli Zacarias <jfzzpro@gmail.com>
2024-06-13 17:41:16 -0600

    Se implementa la anotación @query, y agregamos más métodos, para consultar, agregar y borrar productos

--- Curso_de_Java_Spring1/platzi-market/platzi-market/src/main/java/com/platzi/platzi_market/persistance/ProductoRepository.java
+++ Curso_de_Java_Spring1/platzi-market/platzi-market/src/main/java/com/platzi/platzi_market/persistance/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.platzi.platzi_market.persistance;
 
 import com.platzi.platzi_market.persistance.crud.ProductoCrudRepository;
 import com.platzi.platzi_market.persistance.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     public List<Producto> getAll(){
@@ -21,5 +23,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){ //con este metodo ya puedo obtener un producto en base a su id
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){//metodo para subir un producto
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){ //borra un producto por medio de su id
+        productoCrudRepository.deleteById(idProducto);
     }
 }
