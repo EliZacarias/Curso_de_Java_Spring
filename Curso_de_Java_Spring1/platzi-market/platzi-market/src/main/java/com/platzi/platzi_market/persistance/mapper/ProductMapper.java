62e2143d1b9401c7d67ee3071d7343a3d1f530ea
Eli Zacarias <jfzzpro@gmail.com>
2024-06-23 19:51:00 -0600

    En esta rama se crearon los mapaeadores para convertir Productos en products y categorias en categorys

---
+++ Curso_de_Java_Spring1/platzi-market/platzi-market/src/main/java/com/platzi/platzi_market/persistance/mapper/ProductMapper.java
@@ -0,0 +1,32 @@
+package com.platzi.platzi_market.persistance.mapper;
+
+import com.platzi.platzi_market.domain.Product;
+import com.platzi.platzi_market.persistance.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "Spring", uses = {CategoryMapper.class}) //como estamos usando category y ya tiene su mapper
+                                                                // propio agregamos el uses para agregar la clase de categoria ocupada para mapear
+public interface ProductMapper {
+    @Mappings({
+            @Mapping(source = "idProducto", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "precioVenta", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "categoria", target = "category"),
+
+    })
+    Product toProduct(Producto producto);
+    List<Product> toProducts(List<Producto> productos);
+
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);
+}
